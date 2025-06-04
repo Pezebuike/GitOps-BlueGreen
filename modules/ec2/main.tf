b65a15675691d817b40cd6ccf4f1e5f8215d2e53
Ezebuike Michael <ezebuikemichael@gmail.com>
2025-06-04 22:28:40 +0100

    added install-web.sh

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -82,7 +82,7 @@ output "output_v3_4" {
 resource "aws_instance" "myec2vm" {
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
-  user_data              = file("${path.module}/app1-install.sh")
+  user_data              = file("${path.module}/install-web.sh")
   key_name               = var.instance_keypair
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   
